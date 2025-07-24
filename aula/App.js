407eae72ef0dd8602070c36e20116012f548716b
Bruno Leão <bruno.melosh@gmail.com>
2025-07-24 17:56:53 -0400

    iniciando um projeto para emular com expo go

---
+++ aula/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Vamos testar a aplicação usando o React Native</Text>
+      <Text>O arquivo App.js foi criado</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
